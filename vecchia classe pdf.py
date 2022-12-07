5939c6b1caab1a8dfa0c19d2edb23c73a25793cb
Renato Esposito <53092291+RenatoEsposito1999@users.noreply.github.com>
2022-12-07 21:31:50 +0100

    Few Changes
    not important

--- vecchia classe pdf.py
+++ vecchia classe pdf.py
@@ -1,28 +1,32 @@
+from PyPDF2 import PdfFileReader
 import fitz 
 #Valutare di usare fitz per gestire i PDF. Fitz dovrebbe contenere PyMuPDF.
 class PDF:
     #The pdf file reader
-    _doc = None
+    _parser = None
     def __init__(self,path):
-        _doc = fitz.open(path)
+        _parser = PdfFileReader(path)
         #For each page of the pdf
-        for page in _doc:
-            for annot in page.annots():
-                print(annot.get_text())        
+        for page in _parser.pages:
+            try:
+                for annot in page["/Annots"]:
+                    print(annot.getObject()['/Contents'])
+            except:
+                pass        
+
+        doc = fitz.open(path)  # open document
         i = 0
-        for page in _doc:
+        for page in doc:
             pix = page.get_pixmap()  # render page to an image
-            #pix.save(f"page_{i}.png")
+            pix.save(f"page_{i}.png")
             i+=1
         exit()
 
 
 '''
 SITUAZIONE ATTUALE:
-Riesco a ricavare le foto delle pagine.
+Riesco a etrarre le note dalle pagine e riesco a ricavare le foto delle pagine.
 COSE DA FARE:
-- Estrarre il testo dalle annotazioni, credo che devo lavorare su annot ma ci sono anche altre cose:
-leggi : https://pymupdf.readthedocs.io/en/latest/tutorial.html WORKING WITH PAGE
 - Modificare il size delle foto da codice (in futoro sarà un parametro dell'app)
 - Usare PyMuPDF al posto di PyPDF2 per ottenere le annotazioni.
 - Numerare le foto in base all'indice che va da 0 al numero della foto. 
