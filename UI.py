f5e91ddacf3eeec2a5a860b5d6b2baa59049199a
Renato Esposito <53092291+RenatoEsposito1999@users.noreply.github.com>
2022-12-29 19:47:32 +0100

    ui

--- UI.py
+++ UI.py
@@ -1,7 +1,13 @@
-import PyQt5
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 import sys
-app = QApplication(sys.arg)
-window = QWidget()
-window.setWindowTitle("University Tool Helper")
-window.show()
-sys.exit(app.exec())
+
+class UI:
+    def __init__(self) -> None:
+        app = QApplication(sys.argv)
+        window = QWidget()
+        window.setWindowTitle("University Tool Helper")
+        window.show()
+        sys.exit(app.exec())
+    
--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from PdfController import PdfController
 from BatchPdfController import BatchPdfController
-import os
-import sys
+from UI import UI
 if __name__ == '__main__':
     batch = True
+    ui = UI()
     if not batch:
-        #app = QtWidgets.QApplication(sys.argv)
-        #w = UI()
         try:
             converter =  PdfController('./PDF/Lezione 3.pdf')
         except OSError as error:
             print(error)
             exit()
         converter.convert()
-        #app.exec()
     else:
         batchController = BatchPdfController('./PDF/')
         batchController.convert()
